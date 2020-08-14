a0798283e855808452eae1ca4c2137f30d170ea6
will <will-liu@live.com>
2020-08-14 11:23:24 +0800

    add or update some content

--- autoUpload.sh
+++ autoUpload.sh
@@ -2,3 +2,11 @@ update=`git status -s`
 
 # echo update
 echo $update
+if [ "$update" ==  "" ]
+then
+  echo "empty"
+else
+  echo "contain some add or modify"
+  git commit -a -m "add or update some content"
+  git push
+fi
